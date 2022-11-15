1d79f24d605505fea614533161d68dacc01ce746
Franklin <franklinblanco@tutanota.com>
2022-11-15 11:27:19 -0400

    Commented intoValue macro

--- src/derive/value.rs
+++ src/derive/value.rs
@@ -3,8 +3,7 @@ use proc_macro2::TokenStream;
 use quote::quote;
 use syn::{Data, Field, FieldsNamed, FieldsUnnamed, Ident};
 
-/// Implement
-#[allow(dead_code, unused)] //TODO: Change to from_object
+#[allow(dead_code, unused)]
 pub fn expand_derive_from_value(ident: Ident, data: Data) -> syn::Result<TokenStream> {
     let fields: Vec<Field> = match data {
         syn::Data::Struct(s) => match s.fields {
@@ -47,11 +46,48 @@ pub fn expand_derive_from_value(ident: Ident, data: Data) -> syn::Result<TokenSt
     Ok(output.into())
 }
 
-#[allow(dead_code, unused)]
+/*#[allow(dead_code, unused)]
 pub fn expand_derive_into_value(input: TokenStream) -> syn::Result<TokenStream> {
-    Ok(input)
-}
+    let fields: Vec<Field> = match data {
+        syn::Data::Struct(s) => match s.fields {
+            syn::Fields::Named(FieldsNamed { named, .. }) => named.into_iter().collect(),
+            syn::Fields::Unnamed(FieldsUnnamed { unnamed, .. }) => return Err(syn::Error::new(
+                ident.span(),
+                "Tuple structs are not yet supported.",
+            )),
+            syn::Fields::Unit => {
+                return Err(syn::Error::new(
+                    ident.span(),
+                    "Unit structs are not yet supported.",
+                ))
+            }
+        },
+        _ => return Err(syn::Error::new(ident.span(), "Only structs are allowed.")),
+    };
+    let constructor: Vec<TokenStream> = fields.into_iter().map(|field| destructure_field(field.ident.unwrap())).collect();
+    
+    let output = quote! {
+    impl From<#ident> for surrealdb::sql::Value{
+        type Error = surrealdb::Error;
 
+        fn try_from(value: surrealdb::sql::Value) -> Result<Self, Self::Error> {
+            match value {
+                surrealdb::sql::Value::Object(object) => Ok(Self { #(#constructor), * }),
+                surrealdb::sql::Value::Array(ref array) => {
+                    if array.len() > 0 {
+                        return Self::try_from(array.get(0).unwrap().clone());
+                    } else {
+                        return Err(surrealdb::Error::TryFromError(value.to_string(), "Value contains an empty array"));
+                    }
+                },
+                _ => Err(surrealdb::Error::TryFromError(value.to_string(), "Value not an object")),
+            }
+        }
+    }
+    };
+
+    Ok(output.into())
+} */
 
 fn destructure_field(field_ident: Ident) -> TokenStream {
     let str_field_name = field_ident.to_string();
--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,13 @@ pub fn from_value(input: TokenStream) -> TokenStream {
     }
 }
 
+/* 
 #[proc_macro_derive(ToValue)]
 pub fn to_value(input: TokenStream) -> TokenStream {
+    let DeriveInput { ident, data, .. } = parse_macro_input!(input);
+    match expand_derive_from_value(ident, data) {
+        Ok(tokenstream) =>  tokenstream.into(),//panic!("{}", tokenstream.to_string()),
+        Err(e) => e.into_compile_error().into(),
+    }
     input
-}
+}*/
